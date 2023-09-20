fb2894e5e8109d46249d48cba34b534c99d7effa
pran2711 <pranavthorve27@gmail.com>
2023-09-20 23:50:15 +0530

    day3

---
+++ day3/code.cpp
@@ -0,0 +1,30 @@
+// Q1. Two numbers are given, print their product
+
+#include<iostream>
+using namespace std;
+
+int main(){
+  int a = 2 , b = 4;
+  cout<<a*b;
+}
+
+// Q2. Two numbers are given a and b, print a-b
+
+int main(){
+  int a = 2 , b = 4;
+  cout<<a-b;
+}
+
+// Q3. Print 6*3 in output, don’t print 18, just 6*3 should be in the output screen.
+int main(){
+  cout<<"6*3";
+}
+
+// Q4. If we have only 4 bits, How 3 and -6 will be written in 4 bits.
+// 3 => 4 bit binary = 0011
+// -6 => 4 bit binary = 1010
+
+// Q4. If we have only 5 bits, How 13 and -16 will be written in 5 bits.
+
+//  13 => 5 bit binary = 01101
+//  -16 => 5 bit binary = 10000
